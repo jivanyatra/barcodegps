40fe9285f5fc04ea2383d2cd11e7a22e1ba9938d
Yatri Trivedi <jivanyatra@gmail.com>
2014-10-08 20:32:07 -0400

    failed at doing work today

--- barcodegps.py
+++ barcodegps.py
@@ -1,4 +1,9 @@
-''' This is an application that allows us to quickly use a barcode
+""" This is an application that allows us to quickly use a barcode
     scanner for inputting info into a CSV file for programming our
     GPS units. Hopefully, it will include a feature to scrape a
-    website for the SOS validation post-programming (eventually).'''
+    website for the SOS validation post-programming (eventually).
+"""
+
+import re
+
+
