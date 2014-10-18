dd73e9f7c10f2aa7cc30e82112335d31fdb133ff
Yatri Trivedi <jivanyatra@gmail.com>
2014-10-18 02:51:47 -0400

    created functions, changed structure to be like an actual program, and wrote comments/docstrings for functions and stuff

--- barcodegps.py
+++ barcodegps.py
@@ -5,54 +5,80 @@
 """
 
 import re
-#import scrape
+import scrape
 
-running = True
-while running:
-    print "Welcome to the barcodegps app! Enter a command,\n"
-    print "scan an imei/sim, or type help or ? for a list."
-    inp = raw_input('==> ')
-    if inp == 'q' or inp == 'quit':
-        quit()
-    elif inp == 'clear':
-        scrape.clearlisting()
-#    elif inp == regex for imei:
-#        addimei()
-    elif inp == '
-""" Trying to do this stuff in a dictionary-based way...
-
-inp = raw_input('==> ')
-
-if imei:
-if sim:
-else:
-
-commands: { 'clear' : scrape.clearlisting,
+def checkinput(input):
+    commands: { 'clear' : scrape.clearlisting,
             'v' : validate,
             'validate' : validate,
-            's' : showdata,
             'r' : removeline,
             'd' : removeline,
             'spoof' : spoofvalidation
-}
-validate()
-showdata()
-removeline()
-spoofvalidation()
-#example commands['spoof']()
-"""
-"""This function gets the return page from the scrape function, stores it
-in 'data' and then parses it, searching for a line that matches the imei,
-and has a GPS code of 7011, which is the SOS function button we use to
-validate proper programming of units. Then, it'll store it along with the
-imei and sim numbers.
-"""
+            'q' : savexit
+            'quit' : savexit
+            'h' : help
+            'help' : help
+    }
+    try:
+        commands[input]()
+    except:
+        print "Invalid input. Please try something else, or ask for help."
+
+def showdisplay():
+    """This function is responsible for displaying data on screen
+    using PrettyTable
+    """
+    pass
 
 def parse_validation():
+    """This function gets the return page from the scrape function, stores it
+    in 'data' and then parses it, searching for a line that matches the imei,
+    and has a GPS code of 7011, which is the SOS function button we use to
+    validate proper programming of units. Then, it'll store it along with the
+    imei and sim numbers.
+    """
     data = scrape.scrape()
     for line in data:
         find = imei
         csvalue = [ x.strip() for x in line.split(',') ]
         if (csvalue[0].endswith(find) == True) and (csvalue[1] == '7011'):
             store = line
+
+def validate():
+    pass
+
+def removeline():
+    """Remove a line of data, probably used when encountering a bad
+    unit or a bad sim, and you need to swap units/sims.
+    """
+    pass
+
+def spoofvalidation(imei):
+    """This recreates the proper validation code for the argument the
+    function was called with. It does NOT scrape the website for the
+    actual validation code, it merely creates it as a string.
+    """
+    pass
+
+def help():
+    """This prints out the list of commands and basic things for how the
+    program works.
+    """
+
+def savexit():
+    pass
+
+if __name__ == "__main__":
+    running = True
+    while running:
+        inp = ''
+        showdisplay()
+        print "Welcome to the barcodegps app! Enter a command,\n"
+        print "scan an imei/sim, or type help or ? for a list."
+        inp = raw_input('==> ')
+        #Trying to do this stuff in a dictionary-based way...
+        if imei:
+        if sim:
+        else:
+            checkinput(inp)
 
