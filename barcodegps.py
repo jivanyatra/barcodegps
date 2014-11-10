210e44e5068f847b05f6713a712cb3cb3f6b0351
jivanyatra <jivanyatra@gmail.com>
2014-11-10 17:57:32 -0500

    properly referenced global variables (for now...)

--- barcodegps.py
+++ barcodegps.py
@@ -20,9 +20,10 @@ simre = r'^[0-9]{19}F*$'
 
 
 def choosefilename():
+    global filename
     """Choose a filename for output, in a csv format
     """
-    msg = "Please choose a filename, or press enter for default" \
+    msg = "Please choose a filename, or press enter for default\n" 
           + "(uses today's date): "
     filename = raw_input(msg)
     if filename[-3:] != 'csv':
@@ -30,8 +31,10 @@ def choosefilename():
 
     if not filename:
         filename = 'spytecgps_'+ date + '.csv'
+    return filename
     
 def getdate():
+    global date
     date = datetime.datetime.today().strftime("%m-%d-%y")
 
 def checkinput(input):
@@ -59,18 +62,15 @@ def checkinput(input):
     except:
         print "Invalid input. Please try something else, or ask for help."
 
-def showdisplay():
+def showinfo():
     """This function is responsible for displaying data on screen
-    using PrettyTable
     """
-    if msg == '':
-        msg = "BARcodeGPS"
-    print msg
+    global imei
+    global sim
     if imei:
         print "IMEI # scanned: " + imei
     if sim:
         print "SIM # scanned: " + sim
-    msg = ''
 
 def validate(imei):
     """This function gets the return page from the scrape function, stores it
@@ -79,25 +79,29 @@ def validate(imei):
     validate proper programming of units. Then, it'll store it along with the
     imei and sim numbers.
     """
+    global valstr
     data = scrape.getpage(sessionurl, uname, passw, clearurl)
     for line in data:
         find = imei
         csvalue = [ x.strip() for x in line.split(',') ]
         if (csvalue[0].endswith(find) == True) and (csvalue[1] == '7011'):
             valstr = line
-            msg = "IMEI # " + find + " validated"
+            print "IMEI # " + find + " validated"
             break
         else:
-            msg = "No validation found! Try again!"
+            print "No validation found! Try again!"
 
 def clear():
     scrape.clearpage(sessionurl, uname, passw, clearurl)
-    msg = "Validation listing page cleared!"
+    print "Validation listing page cleared!"
 
 def removeline():
     """Remove a line of data, probably used when encountering a bad
     unit or a bad sim, and you need to swap units/sims.
     """
+    global imei
+    global sim
+    global valstr
     imei = ''
     sim = ''
     valstr = ''
@@ -108,6 +112,7 @@ def spoofvalidation(imei):
     function was called with. It does NOT scrape the website for the
     actual validation code, it merely creates it as a string.
     """
+    global valstr
     valstr = "www.spytecgps.com :$$" + imei + ",7011,//,::,,,0,54," \
              + "0,1,1672,0,8,1.0,0,0.0,0,31,1,0,0.0 "
 
@@ -116,31 +121,36 @@ def help():
     program works.
     """
 
-def opensavefile():
+def opensavefile(file):
+    global imei
+    global sim
+    global valstr
     if imei and sim and valstr:
-        with open(filename, 'a') as fh:
+        with open(file, 'a') as fh:
             lineout = imei + ',' + sim + ',' + valstr + '\n'
             fh.write(lineout)
         imei = ''
         sim = ''
         valstr = ''
         print "IMEI, SIM, and Validation saved!"
+    else:
+        print "IMEI, SIM, and/or Validation missing; file not saved."
     
 if __name__ == "__main__":
     getdate()
     choosefilename()
     while True:
         inp = ''
-        showdisplay()
         print "Welcome to the barcodegps app! Enter a command,\n"
         print "scan an imei/sim, or type help or ? for a list."
+        showinfo()
         inp = raw_input('==> ')
         #Trying to do this stuff in a dictionary-based way...
         if re.search(imeire, inp):
-            imei = str(inp)
+            global imei = str(inp)
         elif re.search(simre, inp):
-            sim = str(inp)
+            global sim = str(inp)
         else:
             checkinput(inp)
-        opensavefile()
+        opensavefile(filename)
     print "Exiting..."
